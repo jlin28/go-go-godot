c918ac40e76a7cc3946c7b79e70afa2f86df775f
Joyce Lin <joycel78@nycstudents.net>
2026-04-24 22:54:14 -0400

    completed a basic movement thing but only left and right

---
+++ waaaa/character_body_3d.gd
@@ -0,0 +1,53 @@
+extends CharacterBody3D
+
+
+#const SPEED = 5.0
+#const JUMP_VELOCITY = 4.5
+#
+#
+#func _physics_process(delta: float) -> void:
+	## Add the gravity.
+	#if not is_on_floor():
+		#velocity += get_gravity() * delta
+#
+	## Handle jump.
+	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		#velocity.y = JUMP_VELOCITY
+#
+	## Get the input direction and handle the movement/deceleration.
+	## As good practice, you should replace UI actions with custom gameplay actions.
+	#var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	#var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	#if direction:
+		#velocity.x = direction.x * SPEED
+		#velocity.z = direction.z * SPEED
+	#else:
+		#velocity.x = move_toward(velocity.x, 0, SPEED)
+		#velocity.z = move_toward(velocity.z, 0, SPEED)
+#
+	#move_and_slide()
+
+## Called when the node enters the scene tree for the first time.
+#func _ready() -> void:
+	#pass # Replace with function body.
+#
+#
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+	#pass
+
+@export var speed = 14
+@export var fall_acceleration = 75
+
+func _physics_process(delta):
+	var direction = Vector3.ZERO
+	
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+		
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+
+	velocity.x = direction.x * speed
+	
+	move_and_slide()

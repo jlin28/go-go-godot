928a81f189b7b3768ae09e231068e872fb5a8a6f
Christine Chen <zhzhnaicha@gmail.com>
2026-04-25 01:06:58 -0400

    embed current godot export into flask

--- __init__.py
+++ __init__.py
@@ -1,1 +1,15 @@
+from flask import Flask, render_template, request, redirect, url_for
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/play")
+def play():
+    return render_template("play.html")
+
+if __name__ == "__main__":
+  app.debug = True
+  app.run()
